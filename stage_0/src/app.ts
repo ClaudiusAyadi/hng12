a02dcc7e6e3c20e713253d043ec8272e95e92e57
Claudius A. <90831377+ClaudiusAyadi@users.noreply.github.com>
2025-02-01 22:34:39 +0100

    Update app.ts

--- stage_0/src/app.ts
+++ stage_0/src/app.ts
@@ -4,7 +4,7 @@ import { logger } from 'hono/logger';
 import router from './routes';
 
 const app = new Hono();
-app.use('*', cors());
+app.use('*', cors({ origin: '*' }));
 app.use('*', logger());
 
 const routes = app.basePath('/api/v1').route('/', router);
